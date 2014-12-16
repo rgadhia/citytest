38accf2809b108601f4257e508567cac4d1f451b
Ravi Gadhia <rgadhia@pivotal.io>
2014-12-16 14:30:13 -0800

    change version to WC

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Los Angeles!";
+        return "Greetings West Coast!";
     }
 }
