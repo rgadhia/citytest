b2f7fb6ea421da33658a6f24f69d3db475df4fd0
Ravi Gadhia <rgadhia@pivotal.io>
2014-12-16 11:50:50 -0800

    change version to Leaky Roof

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Rainy Night!";
+        return "Greetings Leaky Roof!";
     }
 }
