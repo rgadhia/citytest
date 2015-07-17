f19e0c4f2acc8bb9cd8d50c3045d682a05b06f8a
Ravi Gadhia <rgadhia@pivotal.io>
2015-07-17 07:22:18 -0700

    build manifest first

--- task_gradle_build.sh
+++ task_gradle_build.sh
@@ -12,6 +12,20 @@ release_version=$(cat app-version/number)
 echo $@
 echo $release_version
 
+echo  "---
+applications:
+- name: citytest
+  memory: 512M 
+  instances: 1
+  host: citytest-$release-version
+  path: cities-0.0.1.jar
+  env:
+   JAVA_OPTS: -Djava.security.egd=file:///dev/urandom 
+" > manifest.yml
+
+cat manifest.yml
+
+
 export TERM=${TERM:-dumb}
 gradle build
 
@@ -20,18 +34,7 @@ rm build/libs/cities-0.0.1-SNAPSHOT.jar.original
 
 find . 
 
-echo  "---
-applications:
-- name: citytest
-  memory: 512M 
-  instances: 1
-  host: citytest-${release-version}
-  path: cities-0.0.1.jar
-  env:
-   JAVA_OPTS: -Djava.security.egd=file:///dev/urandom 
-" > manifest.yml
 
-cat manifest.yml
 
 
 echo "Build Complete!"
