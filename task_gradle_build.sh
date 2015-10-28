906ffa0e4070b8391ba4f2b705eb86913f45534c
Ravi Gadhia <rgadhia@pivotal.io>
2015-10-28 09:58:14 -0700

    debug

--- task_gradle_build.sh
+++ task_gradle_build.sh
@@ -4,6 +4,9 @@ echo "Build Starting!"
 
 if [[ ! -f app-version/number ]]; then
   echo "save_deployment_pipeline.sh requires release-version/number to contain the candidate version number"
+
+  ls -al 
+  find .
   exit 1
 fi
 release_version=$(cat app-version/number)
